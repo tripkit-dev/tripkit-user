45e9b9ab85c71e8c54266bed1b18e8c9784d7e0c
신정아 <jeonga@khu.ac.kr>
2022-09-24 18:49:18 +0900

    project init

---
+++ src/main/java/tripkit/tripkituser/TripkitUserApplication.java
@@ -0,0 +1,13 @@
+package tripkit.tripkituser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripkitUserApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TripkitUserApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/tripkit/tripkituser/TripkitUserApplicationTests.java
@@ -0,0 +1,13 @@
+package tripkit.tripkituser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripkitUserApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
